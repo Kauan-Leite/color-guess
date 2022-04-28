e1a1b87ed03ffda6a91edff160b9aaf034003f5d
Kauan <kauan.s.leite@hotmail.com>
2022-04-28 18:14:20 -0300

    5° Requisito

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ function addRespostas(){
   for (let index = 0; index < 6; index += 1){
     let circulo = document.createElement('div');
     circulo.classList.add('ball');
+    circulo.classList.add('wrong')
     divRespostas.appendChild(circulo);
   }
 }
@@ -27,14 +28,37 @@ colorBall();
 
 
 
-
-let randomAnswer = Math.floor(Math.random() * 10);
-if(randomAnswer > 5){
-  randomAnswer = 5;
+function correct(){
+  let randomAnswer = Math.floor(Math.random() * 10);
+  if(randomAnswer > 5){
+    randomAnswer = 5;
+  }
+  
+  let correta = document.getElementsByClassName('ball')[randomAnswer];
+  correta.style.backgroundColor = 'rgb' + rgbResposta;
+  
+  correta.classList.remove('wrong');
+  correta.classList.add('correct');
+  
 }
-
-let correta = document.getElementsByClassName('ball')[randomAnswer];
-correta.style.backgroundColor = 'rgb' + rgbResposta;
+correct();
 
 
+
+
+
+let returnAnswer = document.getElementById('answer')
+let allBals = document.getElementsByClassName('ball');
+
+for(let index = 0; index < allBals.length; index += 1){
+  function resposta(){
+    if(allBals[index].classList.contains('wrong')){
+      returnAnswer.innerHTML = 'Errou! Tente novamente!'
+    }
+    else if(allBals[index].classList.contains('correct')){
+      returnAnswer.innerHTML = 'Acertou!'
+    }
+  }
+  allBals[index].addEventListener('click', resposta)
+}
 
