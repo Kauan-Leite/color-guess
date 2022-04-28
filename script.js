cf8f16b45cedd71a1552020677ddebb6cc69b8a3
Kauan <kauan.s.leite@hotmail.com>
2022-04-28 17:55:49 -0300

    3° e 4° Requisito

--- script.js
+++ script.js
@@ -1,5 +1,40 @@
-cor = document.getElementById('rgb-color');
+let cor = document.getElementById('rgb-color');
+let divRespostas = document.getElementById('respostas');
 
-resposta = '(' + Math.floor(Math.random() * 255) + ', ' + Math.floor(Math.random() * 255) + ', ' + Math.floor(Math.random() * 255) + ')';
+rgbResposta = '(' + Math.floor(Math.random() * 255) + ', ' + Math.floor(Math.random() * 255) + ', ' + Math.floor(Math.random() * 255) + ')';
 
-cor.innerHTML = resposta;
+cor.innerHTML = rgbResposta;
+
+
+function addRespostas(){
+  for (let index = 0; index < 6; index += 1){
+    let circulo = document.createElement('div');
+    circulo.classList.add('ball');
+    divRespostas.appendChild(circulo);
+  }
+}
+addRespostas();
+
+
+function colorBall(){
+  for (let index = 0; index < 6; index += 1){
+    let bolas = document.getElementsByClassName('ball')[index];
+    bolas.style.backgroundColor = 'rgb(' + Math.floor(Math.random() * 255) + ', ' + Math.floor(Math.random() * 255) + ', ' + Math.floor(Math.random() * 255) + ')';
+  }
+}
+colorBall();
+
+
+
+
+
+let randomAnswer = Math.floor(Math.random() * 10);
+if(randomAnswer > 5){
+  randomAnswer = 5;
+}
+
+let correta = document.getElementsByClassName('ball')[randomAnswer];
+correta.style.backgroundColor = 'rgb' + rgbResposta;
+
+
+
