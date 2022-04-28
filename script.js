01b3b5981937cdc8e331b76590fe8547b23979b9
Kauan <kauan.s.leite@hotmail.com>
2022-04-28 17:29:08 -0300

    2° Requisito

---
+++ script.js
@@ -0,0 +1,5 @@
+cor = document.getElementById('rgb-color');
+
+resposta = '(' + Math.floor(Math.random() * 255) + ', ' + Math.floor(Math.random() * 255) + ', ' + Math.floor(Math.random() * 255) + ')';
+
+cor.innerHTML = resposta;
